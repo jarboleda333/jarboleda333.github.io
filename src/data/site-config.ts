ecf7e06aa0bb980d35ffe412a8b0b722affe6799
jarboleda333 <146093214+jarboleda333@users.noreply.github.com>
2025-05-29 18:28:40 -0500

    Update home

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -25,8 +25,6 @@ export type Subscribe = {
 export type SiteConfig = {
     website: string;
     logo?: Image;
-    title: string;
-    subtitle?: string;
     description: string;
     image?: Image;
     headerNavLinks?: Link[];
@@ -40,8 +38,6 @@ export type SiteConfig = {
 
 const siteConfig: SiteConfig = {
     website: 'https://jarboleda333.github.io',
-    title: 'Jorge Arboleda',
-    subtitle: 'Analista y Desarrollador de Software',
     description: 'Tema para blog y portafolio basado en Astro.js y Tailwind CSS, creado por justgoodui.com',
     
     headerNavLinks: [
