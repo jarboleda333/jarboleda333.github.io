36eb8f876a2c9e434bae4a3304d57974e5eb8d20
jarboleda333 <146093214+jarboleda333@users.noreply.github.com>
2025-05-29 20:21:06 -0500

    Update home

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,21 +1,12 @@
-import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content';
-import siteConfig from '../data/site-config.ts';
-import { sortItemsByDateDesc } from '../utils/data-utils.ts';
-
-export async function GET(context) {
-    const posts = (await getCollection('blog')).sort(sortItemsByDateDesc);
-
-    return rss({
-        title: siteConfig.title || 'Mi RSS', // Valor por defecto si no hay title
-        description: siteConfig.description || 'Descripción del feed RSS', // Valor por defecto
-        site: context.site,
-        items: posts.map((item) => ({
-            title: item.data.title,
-            description: item.data.excerpt,
-            link: `/blog/${item.id}/`,
-            pubDate: item.data.publishDate.setUTCHours(0),
-        })),
-    });
-}
+return rss({
+    title: siteConfig.title || '',         // Vacío si no lo hay
+    description: siteConfig.description || '', // Vacío si no lo hay
+    site: context.site,
+    items: posts.map((item) => ({
+        title: item.data.title,
+        description: item.data.excerpt,
+        link: `/blog/${item.id}/`,
+        pubDate: item.data.publishDate.setUTCHours(0),
+    })),
+});
 
