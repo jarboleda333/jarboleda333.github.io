3785b3663e86ccd9a4091aa048751155b543ac91
jarboleda333 <146093214+jarboleda333@users.noreply.github.com>
2025-05-29 20:22:40 -0500

    Update home

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,12 +1,21 @@
-return rss({
-    title: siteConfig.title || '',         // Vacío si no lo hay
-    description: siteConfig.description || '', // Vacío si no lo hay
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+import siteConfig from '../data/site-config';
+import { sortItemsByDateDesc } from '../utils/data-utils';
+
+export async function GET(context) {
+  const posts = (await getCollection('blog')).sort(sortItemsByDateDesc);
+
+  return rss({
+    title: siteConfig.title || '',
+    description: siteConfig.description || '',
     site: context.site,
     items: posts.map((item) => ({
-        title: item.data.title,
-        description: item.data.excerpt,
-        link: `/blog/${item.id}/`,
-        pubDate: item.data.publishDate.setUTCHours(0),
+      title: item.data.title,
+      description: item.data.excerpt,
+      link: `/blog/${item.id}/`,
+      pubDate: item.data.publishDate.setUTCHours(0),
     })),
-});
+  });
+}
 
